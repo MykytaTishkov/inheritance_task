1e6ad9810941aa51747f2588307f4541b689b9ce
MykytaTishkov <mykyta.tishkov@sigma.software>
2021-03-10 14:41:17 +0200

    Completing c inherritance

--- c_inher.c
+++ c_inher.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-typedef struct A
+typedef struct
 {
+    char Name_index;
+} Parent;
 
+typedef struct
+{
+    Parent P;
+} Child;
+
+int main()
+{
+    Parent *A;
+    Child *B;
+    B = calloc(1, sizeof(Parent));
+    B->P.Name_index = 'C';
+    A = (Child*)B;
+    printf("I`m %c\n", A->Name_index);
 }
